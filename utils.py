681ec3ef88353763a33ba7356a84c7a57092713d
Goose4me <gamolyaruslan@gmail.com>
2020-02-14 11:19:37 +0200

    Make redirection for "127.0.0.1:8000/"

--- utils.py
+++ utils.py
@@ -8,37 +8,45 @@ import mimetypes
 import falcon
 from TrainModel import *
 
+
 class Train():
     accurancy = 0
+
     def on_get(self, req, resp):
         if self.accurancy == 0:
             self.accurancy = train(0)
         resp.status = falcon.HTTP_200
-        resp.body = json.dumps({'accurancy':self.accurancy})
-
+        resp.body = json.dumps({'accurancy': self.accurancy})
 
     def on_post(self, req, resp):
         epochs = req.bounded_stream.read(req.content_length)
         print(req.bounded_stream.read(req.content_length))
         self.accurancy = train(int(epochs))
         resp.status = falcon.HTTP_200
-        resp.body = json.dumps({'accurancy':self.accurancy})
-        
+        resp.body = json.dumps({'accurancy': self.accurancy})
+
+
+class Redirection(object):
+    def on_get(self, req, res):
+        res.content_type = "text/html"
+        res.status = falcon.HTTP_200
+        res.stream = open("templates/main.html", "rb")
 
 
 class Resource(object):
     image_path = "/img"
     prediction = -1
+
     def __init__(self, app):
         self.app = app
+
     def on_get(self, req, resp):
         if self.image_path != "/img":
             self.prediction = make_prediction(self.image_path)
         else:
             self.prediction = "No photo"
         resp.status = falcon.HTTP_200
-        resp.body = json.dumps({"prediction":str(self.prediction), "path":self.image_path})
-
+        resp.body = json.dumps({"prediction": str(self.prediction), "path": self.image_path})
 
     def on_post(self, req, resp):
         ext = mimetypes.guess_extension(req.content_type)
@@ -51,9 +59,8 @@ class Resource(object):
         print(req.bounded_stream.read())
         resp.status = falcon.HTTP_200
         resp.location = '/images/' + name
-        self.app.add_route("/"+self.image_path.replace("\\","/"), GetImg(self.image_path))
-        resp.body = json.dumps({"prediction":str(self.prediction), "path":self.image_path.replace("\\","/")})
-
+        self.app.add_route("/" + self.image_path.replace("\\", "/"), GetImg(self.image_path))
+        resp.body = json.dumps({"prediction": str(self.prediction), "path": self.image_path.replace("\\", "/")})
 
 
 class Img():
@@ -72,6 +79,7 @@ class JQUERY():
         res.status = falcon.HTTP_200
         res.stream = open("resources/jquery-3.2.1.min.js", "rb")
 
+
 # class Main():
 #     def on_get(self,req,res):
 #         res.content_type = "text/html"
@@ -85,9 +93,11 @@ class TrainPage():
         res.status = falcon.HTTP_200
         res.stream = open("templates/train.html", "rb")
 
+
 class GetImg():
     def __init__(self, path):
         self.path = path
+
     def on_get(self, req, res):
         res.status = falcon.HTTP_200
         res.stream = open(self.path, "rb")
